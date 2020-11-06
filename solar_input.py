0fafed2c3e6086bacad168bd585ddae36ad08ccd
Sashashen <shentsev.am@phystech.edu>
2020-11-06 18:25:48 +0300

    A2

--- solar_input.py
+++ solar_input.py
@@ -19,13 +19,16 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
-
     return objects
 
 
@@ -43,8 +46,16 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+    parameters = line.split()
+    star.R = int(parameters[1])
+    star.color = parameters[2]
+    star.m = float(parameters[3])
+    star.x = float(parameters[4])
+    star.y = float(parameters[5])
+    star.Vx = float(parameters[6])
+    star.Vy = float(parameters[7])
 
-    pass  # FIXME: not done yet
+
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,7 +72,15 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    parameters = line.split()
+    planet.R = int(parameters[1])
+    planet.color = parameters[2]
+    planet.m = float(parameters[3])
+    planet.x = float(parameters[4])
+    planet.y = float(parameters[5])
+    planet.Vx = float(parameters[6])
+    planet.Vy = float(parameters[7])
+    print(planet.Vy)
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
