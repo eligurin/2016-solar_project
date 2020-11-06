90a4ff7482241041c405f4403620c66f2090eaf5
Sashashen <shentsev.am@phystech.edu>
2020-11-06 17:23:32 +0300

    А1

--- solar_model.py
+++ solar_model.py
@@ -32,8 +32,11 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
+    body.x += body.Vx  # FIXME: не понимаю как менять...
     body.Vx += ax*dt
+    ay = body.Fy/body.m
+    body.y += body.Vy  # FIXME: не понимаю как менять...
+    body.Vy += ay*dt
     # FIXME: not done recalculation of y coordinate!
 
 
